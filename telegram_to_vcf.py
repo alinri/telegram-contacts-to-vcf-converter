3006f15fbc1b5f07c93c3914b0d9bf7a567861ad
pickle-rick <13noori79@gmail.com>
2021-02-02 17:03:44 +0330

    Fix typo error.

--- telegram_to_vcf.py
+++ telegram_to_vcf.py
@@ -29,4 +29,4 @@ for contact in contacts_dict:
 
 result_vcf.close()
 print('done')
-print(f'Your vcf file Stored in in {path.dirname(__file__)+path.sep}result.vcp')
+print(f'Your vcf file Stored in in {path.dirname(__file__)+path.sep}result.vcf')
