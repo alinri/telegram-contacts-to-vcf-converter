956f98f6f12d40fef3560356e7b7c0f3d311c17e
pickle-rick <13noori79@gmail.com>
2021-02-02 16:18:38 +0330

    Close contact_file.

--- telegram_to_vcf.py
+++ telegram_to_vcf.py
@@ -6,3 +6,5 @@ file_location = input("Please enter your telegram json location:\n")
 contact_file = io.open(file_location, 'r', encoding='utf-8')
 
 contact_raw = ''.join(contact_file.readlines())
+
+contact_file.close()
