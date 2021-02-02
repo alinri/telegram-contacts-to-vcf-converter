4d2ea7c949fe3e7e48573b89b6d85f290dd3b711
pickle-rick <13noori79@gmail.com>
2021-02-02 16:18:06 +0330

    Read contact_file and store in contact_raw.

--- telegram_to_vcf.py
+++ telegram_to_vcf.py
@@ -4,3 +4,5 @@ import io
 file_location = input("Please enter your telegram json location:\n")
 
 contact_file = io.open(file_location, 'r', encoding='utf-8')
+
+contact_raw = ''.join(contact_file.readlines())
